85e8ad6c0d6df7add440029f4db015fab2215c3c
“kieuvu” <kieuvuvuvu@gmail.com>
2023-06-18 00:15:44 +0900

    base method

---
+++ lib/index.d.ts
@@ -0,0 +1,22 @@
+interface IDateUtils {
+}
+declare abstract class DateUtilsImplement implements IDateUtils {
+    protected _dateTime: Date;
+    constructor(dateTime?: string | null);
+    private _init;
+    static getCurrentTime: () => Date;
+    static isValidDateTime: (datetimeString: any) => boolean;
+    static getDaysInMonth: (month?: number, year?: number) => number;
+    static isLeapYear: (year?: number) => boolean;
+    static getDaysInYear: (year?: number) => number;
+    static isToday: (dateTimeString: Date | string) => boolean;
+}
+declare class DateUtils extends DateUtilsImplement {
+    constructor(_dateTime?: string | null);
+    getDateTime: () => Date;
+    getDaysInMonth: () => number;
+    isLeapYear: () => boolean;
+    getDaysInYear: () => number;
+    isToday: () => boolean;
+}
+export default DateUtils;
